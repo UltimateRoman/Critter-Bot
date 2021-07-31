dd2a006f02c46e05225a1850ba553c0cb9a925cc
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2021-07-31 18:52:41 +0530

    Fetches new messages

--- app.py
+++ app.py
@@ -2,3 +2,31 @@ import time, requests, json, urllib
 
 token = input("Enter telegram bot token:")
 url = "https://api.telegram.org/bot"+token
+
+
+def get_message(offset):
+    if offset:
+        resp = requests.get(url+"/getUpdates?timeout=100&offset={}".format(offset))
+    else:
+        resp = requests.get(url+"/getUpdates?timeout=100")
+    respd = json.loads(resp.content.decode("utf8"))
+    return respd
+
+
+def main():
+    prev_id = None
+    t1 = time.time()
+    while True:
+        try:
+            t2 = time.time()
+            if t2-t1>20:
+                t1 = time.time()
+            respn = get_message(prev_id)
+            
+        except Exception as e:
+            print(e)
+            time.sleep(5)
+
+
+if __name__ == '__main__':
+    main()
