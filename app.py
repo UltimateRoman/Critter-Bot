7de4d0f359965de208f6bf0aed2cc27ec785cecb
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2021-07-31 21:17:37 +0530

    Changes in app.py

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ def get_message(offset):
     respd = json.loads(resp.content.decode("utf8"))
     return respd
 
+def get_last_id(respd):
+    ids=[]
+    for msg in respd['result']:
+        ids.append(int(msg['update_id']))
+    return max(ids)
+
+
 
 def main():
     prev_id = None
@@ -21,7 +28,11 @@ def main():
             t2 = time.time()
             if t2-t1>20:
                 t1 = time.time()
-            respn = get_message(prev_id)
+            respd = get_message(prev_id)
+            if len(respd['result'])>0:
+                prev_id = get_last_id(respd)+1
+                make_msg(respd)
+            time.sleep(1)
             
         except Exception as e:
             print(e)
